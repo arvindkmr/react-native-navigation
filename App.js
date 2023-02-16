fc1800bec6fa3e752342a71db42af192db3fc37c
Arvind <arvindkmr003@gmail.com>
2023-02-16 17:08:20 +0530

    running app

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {Button, View,StyleSheet, Text} from 'react-native';
 import Navig from './Src/Navigate/Navig';
-
 function App() {
   return (
     <View style={{ flex: 1 }}>
